5d7ee6de83e5d0326daf1f69d7f1f86e4cb6a754
Z.Guluzade <ziyad_guluzade@outlook.com>
2019-06-22 14:43:16 -0400

    set up the game handling functions and if else statements for every players choice of rock paper scissors

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+// assigning all elements with the ghoice tag to a variable
+const choices = document.querySelectorAll('.choice');
+
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.getElementById('.modal');
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
+
+// Play game function
+function play(e) {
+    restart.style.display = 'inline-block';
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+}
+
+function getComputerChoice() {
+    const rand = Math.random();
+    if(rand < 0.34) {
+        return 'rock';
+    } else if (rand <= 0.67) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+
+function getWinner(p, c) {
+    if(p ===c) {
+        return 'draw';
+    } else if(p === 'rock') {
+        if (c === 'paper') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    } else if(p === 'paper') {
+        if (c === 'scissors') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    } else if(p === 'scissors') {
+        if(c === 'rock') {
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    }
+}
+// event listeners
+choices.forEach(choice => choice.addEventListener('click', play));
